255fc80f3804792abb95d44f7cb01bec12a5ca0b
Soya-Onishi <p5h60esu@s.okayama-u.ac.jp>
2018-03-25 23:10:07 +0900

    add
    add new file for stack instructions

---
+++ stack_inst.c
@@ -0,0 +1,25 @@
+#include"cpu_circuit.h"
+#include"memory.h"
+#include"status_flag_manager.h"
+
+int pha_implied() {
+  memory[registers.stack--] = registers.accumulator;
+  return 0;
+}
+
+int pla_implied() {
+  registers.accumulator = memory[++registers.stack];
+  set_z_flag(registers.accumulator);
+  set_n_flag(registers.accumulator);
+  return 0;
+}
+
+int php_implied() {
+  memory[registers.stack--] = registers.status | STATUS_B;
+  return 0;
+}
+
+int plp_implied() {
+  registers.status = memory[++registers.stack];
+  return 0;
+}
