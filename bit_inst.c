ef0bf1a2ada81ef2d438acb92fd8999917bc319b
Soya-Onishi <p5h60esu@s.okayama-u.ac.jp>
2018-03-25 17:23:10 +0900

    add
    add new file for bit instruction

---
+++ bit_inst.c
@@ -0,0 +1,41 @@
+#include"cpu_circuit.h"
+#include"memory.h"
+#include"status_flag_manager.h"
+
+void exec_bit(unsigned char data) {
+  unsigned char result;
+
+  result = registers.accumulator &  data;
+  
+  set_z_flag(result);
+  set_n_flag(data);
+  
+  if(data & STATUS_V) {
+    registers.status |= STATUS_V;
+  } else {
+    registers.status &= ~STATUS_V;
+  }
+}
+
+int bit_zeropage() {
+  unsigned char addr, data;
+
+  addr = memory[registers.pc + 1];
+  data = memory[addr];
+  exec_bit(data);
+
+  return 0;
+}
+
+int bit_absolute() {
+  unsigned short addr;
+  unsigned char data;
+
+  addr = memory[registers.pc + 1];
+  addr |= (unsigned short)memory[registers.pc + 2]  << 8;
+
+  data = memory[addr];
+  exec_bit(data);
+
+  return 0;
+} 
