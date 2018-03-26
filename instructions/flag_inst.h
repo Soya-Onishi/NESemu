c2142185cedeea244b33e8ccce69dd5594d274db
Soya-Onishi <p5h60esu@s.okayama-u.ac.jp>
2018-03-26 14:18:44 +0900

    fix
    flag_instrucionts rename and moved

---
+++ instructions/flag_inst.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_FLAG_INST
+#define HEADER_FLAG_INST
+
+int clc_implied();
+int sec_implied();
+int cli_implied();
+int sei_implied();
+int cld_implied();
+int sed_implied();
+int clv_implied();
+
+#endif
